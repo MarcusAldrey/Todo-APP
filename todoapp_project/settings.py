d715cc37707181133b635a4cbd3299f596f8d83c
Marcus Aldrey <marcusaldrey@gmail.com>
2021-10-11 01:31:20 -0300

    correção allowed hosts

--- todoapp_project/settings.py
+++ todoapp_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "%l@n*s_%+uga^ljbx+rkm)))t_&p&jd*n@l+$gn#944!m=wjhc"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","https://obscure-crag-30514.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","obscure-crag-30514.herokuapp.com"]
 
 
 # Application definition
