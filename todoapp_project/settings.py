8d1ecf5f29256e31013e6836e663ad48eaa12b84
Marcus Aldrey <marcusaldrey@gmail.com>
2021-10-11 01:29:03 -0300

    adiciona heroku a allowed hosts

--- todoapp_project/settings.py
+++ todoapp_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "%l@n*s_%+uga^ljbx+rkm)))t_&p&jd*n@l+$gn#944!m=wjhc"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","https://obscure-crag-30514.herokuapp.com"]
 
 
 # Application definition
