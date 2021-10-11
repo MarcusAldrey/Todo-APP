883f71b55783685c5a2dd3642b38886b3565c3b6
Marcus Aldrey <marcusaldrey@gmail.com>
2021-10-11 02:09:45 -0300

    debug true

--- todoapp_project/settings.py
+++ todoapp_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "%l@n*s_%+uga^ljbx+rkm)))t_&p&jd*n@l+$gn#944!m=wjhc"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "obscure-crag-30514.herokuapp.com"]
 
