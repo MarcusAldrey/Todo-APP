e830a39106dae2ef71fa27567d7552bad9057780
Marcus Aldrey <marcusaldrey@gmail.com>
2021-10-11 02:17:08 -0300

    adicionado suporte a postgres no heroku

--- todoapp_project/settings.py
+++ todoapp_project/settings.py
@@ -82,6 +82,11 @@ DATABASES = {
     }
 }
 
+if "DATABASE_URL" in os.environ:
+    import dj_database_url
+
+    DATABASES = {"default": dj_database_url.config()}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
