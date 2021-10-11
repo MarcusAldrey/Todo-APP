0d49efa01d2e060e7e943ae70c0b93b93998085e
Marcus Aldrey <marcusaldrey@gmail.com>
2021-10-11 01:39:30 -0300

    adiciona whitenoise

--- todoapp_project/settings.py
+++ todoapp_project/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "todoapp_project.urls"
@@ -123,3 +124,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "todo/"),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
