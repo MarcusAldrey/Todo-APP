46baa764486608580ff817fcef36fe94f13bf8aa
Marcus Aldrey <marcusaldrey@gmail.com>
2021-10-14 12:17:46 -0300

    remove database settings

--- todoapp_project/settings.py
+++ todoapp_project/settings.py
@@ -82,11 +82,6 @@ DATABASES = {
     }
 }
 
-if "DATABASE_URL" in os.environ:
-    import dj_database_url
-
-    DATABASES = {"default": dj_database_url.config()}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -95,15 +90,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
 
